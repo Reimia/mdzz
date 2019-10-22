6e2212babd034ff02226fea52c78672e93b8b410
zhangsiyu <2944136662@qq.com>
2019-10-22 17:32:09 +0800

    Hello World

---
+++ src/main/java/com/Reimia/BianLi.java
@@ -0,0 +1,50 @@
+package com.Reimia;
+
+import java.io.*;
+
+public class BianLi {
+
+    public static void main(String[] args) {
+        String path = "D:\\bianliceshi";//要遍历的路径
+        String path2 = "D:\\bianliceshi.txt";//输出文件地址(如果没有则创建)
+        File file = new File(path);
+        File fileOut = new File(path2);
+        if (fileOut.exists()) {
+            fileOut.delete();//如果输出文件存在则删除文件
+        }
+        func(file, fileOut);
+    }
+
+    private static void func(File file, File fileOut) {
+        File[] fs = file.listFiles();
+
+        assert fs != null;
+        for (File f : fs) {
+            if (f.isDirectory())    //若是目录，则递归打印该目录下的文件
+            {
+                func(f, fileOut);
+            }
+            if (f.isFile())        //若是文件，直接打印
+            {
+                System.out.println(f);//输出绝对路径
+                try (FileReader reader = new FileReader(f);
+                     BufferedReader br = new BufferedReader(reader)) {
+                    String line;
+
+                    if (!fileOut.exists()) {
+                        fileOut.createNewFile(); // 如果输出文件不知道则创建新文件
+                    }
+                    while ((line = br.readLine()) != null) {
+                        System.out.println(line);//按行输出文本内容
+                        BufferedWriter out = new BufferedWriter(new FileWriter(fileOut, true));
+                        out.write(line);
+                        out.flush();
+                        out.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/Reimia/BianLiErJinZhi.java
@@ -0,0 +1,58 @@
+package com.Reimia;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class BianLiErJinZhi {
+    public static void main(String[] args) {
+        String path = "D:\\bianliceshi";//要遍历的路径
+        String path2 = "D:\\bianliceshi.txt";//输出文件地址(如果没有则创建)
+        File file = new File(path);
+        File fileOut = new File(path2);
+        if (fileOut.exists()) {
+            if (fileOut.delete()){
+                System.out.println("文件已存在，将删除后重新创建");
+                //如果输出文件存在则删除文件
+            }
+        }
+        func(file, fileOut);
+    }
+
+    private static void func(File file, File fileOut) {
+        File[] fs = file.listFiles();
+
+        assert fs != null;
+        for (File f : fs) {
+            if (f.isDirectory())    //若是目录，则递归打印该目录下的文件
+            {
+                func(f, fileOut);
+            }
+            if (f.isFile())        //若是文件，直接打印
+            {
+                System.out.println(f);//输出绝对路径
+                try {
+                    int length = (int) f.length();
+                    FileInputStream fis = new FileInputStream(f);
+                    FileOutputStream fos = new FileOutputStream(fileOut, true);
+
+                    BufferedInputStream in = new BufferedInputStream(fis);
+                    ByteArrayOutputStream out = new ByteArrayOutputStream(length);
+                    byte[] temp = new byte[length];
+                    while (in.read(temp) != -1) {
+                        out.write(temp);
+                    }
+                    byte[] content = out.toByteArray();
+                    System.out.println(Arrays.toString(content));//打印写入内容
+                    fos.write(content);//写入
+                    in.close();
+                    out.close();
+                    fos.close();
+                    fis.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
